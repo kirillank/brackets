9f0f6bc1ca2aba84a7ce1e96a264aafedf22750b
kirillank <kirill.ank04@yandex.by>
2021-03-13 02:56:16 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let result = false
+  str = str.split('');
+  brack(str, bracketsConfig)  
+  function brack(str){
+    if(str.length == 0){
+      result = true;
+      } else {
+        for (let i = 0; i < str.length; i++){
+          for (let j = 0; j < bracketsConfig.length; j++){
+            if(str[i] == bracketsConfig[j][0] && str[i+1] == bracketsConfig[j][1]){
+              str.splice(i, 2);
+              brack(str);
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
